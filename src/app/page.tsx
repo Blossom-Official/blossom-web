a118e0052bdb193d3e971c2f1212ad5f437ee057
Jeongjin Oh <ojj991123@gmail.com>
2023-06-13 16:57:09 +0900

    ➕ add: tailwindcss 패키지 설치 - @link https://tailwindcss.com/docs/guides/nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,28 @@
-'use client';
-import Link from 'next/link';
-import { useState } from 'react';
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
-import { Review } from '@/mocks/types';
+import { Review } from "@/mocks/types";
 
 export default function Home() {
   const [reviews, setReviews] = useState<Review[] | null>(null);
 
   const handleGetReviews = () => {
     // Client-side request are mocked by `mocks/browser.ts`.
-    fetch('/reviews')
+    fetch("/reviews")
       .then((res) => res.json())
       .then(setReviews);
   };
   return (
     <main>
-      <button onClick={handleGetReviews}>Load reviews</button>
-      <Link href='/signin'>Login</Link>
-      <Link href='/logout'>logout</Link>
+      <button
+        className="text-3xl font-bold underline "
+        onClick={handleGetReviews}
+      >
+        Load reviews
+      </button>
+      <Link href="/signin">Login</Link>
+      <Link href="/logout">logout</Link>
       {reviews && (
         <ul>
           {reviews.map((review) => (
