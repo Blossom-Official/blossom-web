053558ba16ba0c4ce78f2ce7a989485612105536
Jeongjin <rojay.developer@gmail.com>
2023-07-06 17:05:16 +0900

    ⚰️ remove(useGetFlowerLikes): 더미 데이터 삭제

--- src/api/flower-like/useGetFlowerLikes.ts
+++ src/api/flower-like/useGetFlowerLikes.ts
@@ -3,32 +3,13 @@ import { useSuspenseQuery } from '@suspensive/react-query';
 import { authHttp } from '../core/axios';
 import { RequestSuccess } from '../core/types';
 
-const imageUrl = 'https://source.unsplash.com/random/300×300';
-
 export const useGetFlowerLikes = () => {
   return useSuspenseQuery({
     queryKey: useGetFlowerLikes.queryKey,
     queryFn: useGetFlowerLikes.queryFn,
     select: (response) => {
       const { data } = response.data;
-      // return data;
-      return {
-        totalCount: 2,
-        flowers: [
-          {
-            flowerId: 1,
-            koreanName: '물망초',
-            englishName: 'FORGET ME NOT',
-            imageUrl,
-          },
-          {
-            flowerId: 2,
-            koreanName: '안개꽃',
-            englishName: 'COMMON GYPSOPHILA',
-            imageUrl,
-          },
-        ],
-      };
+      return data;
     },
   });
 };
