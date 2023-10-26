f939c4f2162eb39a4793c26a77a93183241738d8
Jeongjin <rojay.developer@gmail.com>
2023-10-26 22:28:19 +0900

    remove: 꽃 상세 화면 더미데이터 제거

--- src/api/flower/useGetFlowerDetail.ts
+++ src/api/flower/useGetFlowerDetail.ts
@@ -8,8 +8,7 @@ export const useGetFlowerDetail = (flowerId: string) => {
     queryKey: useGetFlowerDetail.queryKey(flowerId),
     queryFn: () => useGetFlowerDetail.queryFn(flowerId),
     select: (response) => {
-      // const { data } = response.data;
-      const { data } = mockData;
+      const { data } = response.data;
       return data;
     },
   });
@@ -46,52 +45,3 @@ useGetFlowerDetail.queryKey = (flowerId: string) =>
 
 useGetFlowerDetail.queryFn = (flowerId: string) =>
   http.get<RequestSuccess<Response>>(`/flower/${flowerId}`);
-
-const imageUrl = 'https://source.unsplash.com/random/300×300';
-
-const mockData = {
-  code: '',
-  message: '',
-  data: {
-    koreanName: '은방울 꽃',
-    englishName: 'LILY OF THE VALLEY',
-    images: [imageUrl, imageUrl, imageUrl],
-    familyName: '백합과',
-    times: [5, 6],
-    languages: ['순결', '다시 찾은 행복'],
-    tags: ['행복', '사랑', '연인'],
-    additionalInformation:
-      '꽃과 잎에 독성이 있으므로 꽃 장식을 한 후에는 손을 씻어야 한다.',
-    fitName: '압화',
-    fitInformation:
-      '일주일간 책 사이에 꽂아두면 돼요, 압화 전용 시트가 있다면 사용해주세요!',
-    cares: [
-      '첫번째 순서입니다.',
-      '두번째 순서입니다.',
-      '세번째 순서입니다.',
-      '네번째 순서입니다.',
-    ],
-    betterTogethers: [
-      {
-        flowerId: 2,
-        koreanName: '산데르소니아',
-        englishName: 'SANDERSONIA',
-        imageUrl: imageUrl,
-      },
-      {
-        flowerId: 3,
-        koreanName: '레우코코리네',
-        englishName: 'GLORY OF THE SUN',
-        imageUrl: imageUrl,
-      },
-    ],
-    contents: [
-      {
-        contentId: 1,
-        imageUrl: imageUrl,
-        title: 'ONE DAY CLASS',
-        subtitle: '봄에는 꽃과 함께 기분전환',
-      },
-    ],
-  },
-};
